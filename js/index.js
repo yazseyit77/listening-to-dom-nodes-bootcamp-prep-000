b0ea31ad7deabfcb1bd459819a1dea0b5cfca526
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-26 16:14:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
