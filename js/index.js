fb702fa8724a373e3c2602f156b9ff956050b04f
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-03-02 14:36:33 -0800

    Done.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-const main = document.getElementById('main');
- 
-main.addEventListener('click', function(event) {
-  alert('I was clicked!');
-});
+const divs = document.querySelectorAll('div');
 
-const input = document.querySelector('input');
- 
-input.addEventListener('keydown', function(e) {
-  console.log(e.key);
-});
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
